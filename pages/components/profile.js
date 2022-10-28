476f60b29365f312f0b13b83455cd753e5bdf025
emily <eap6787@gmail.com>
2022-10-28 11:52:00 -0600

    add profile pic styles

--- pages/components/profile.js
+++ pages/components/profile.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import profilePicPlaceholder from '../assets/images/profile_pic_placeholder.jpeg';
+import { Icon } from '@iconify/react';
 
 export default function Profile() {
   const [user, setUser] = useState(null);
@@ -17,8 +18,18 @@ export default function Profile() {
   return user ? (
     <>
       <div>
-        <div className='p-10 flex justify-center xl:justify-start lg:justify-start md:justify-start'>
-          <Image className='rounded-full' src={profilePicPlaceholder} width={150} height={150} />
+        <div className='p-10 flex justify-center xl:justify-start lg:justify-start md:justify-start lg:ml-40 md:ml-0'>
+          <div className='rounded-full border-4 pt-1 border-black py-0 px-1 '>
+            <Image
+              className='rounded-full border-2 border-black'
+              src={profilePicPlaceholder}
+              width={150}
+              height={150}
+            />
+          </div>
+          <button className='absolute xl:mt-5 lg:mt-5 md:mt-5 mt-32 rounded-full'>
+            <Icon icon='akar-icons:circle-plus-fill' color='lime' width={50} height={50} />
+          </button>
         </div>
       </div>
     </>
