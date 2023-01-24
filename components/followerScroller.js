b2a0ffd0071433d02d84587d03d3fab00ec9ba2f
emily <eap6787@gmail.com>
2023-01-24 14:10:42 -0700

    add link functionality to the scrollbar of users

--- components/followerScroller.js
+++ components/followerScroller.js
@@ -1,18 +1,21 @@
 import { defaultConfig } from 'next/dist/server/config-shared';
 import React from 'react';
 import Image from 'next/image';
+import Link from './Link';
 
 export default function FollowerScroller({ following }) {
   return (
     <div className='overflow-x-scroll flex max-w-xs lg:max-w-lg md:max-w-lg'>
       {following.map((followers) => {
         return (
-          <div>
-            <div className='flex flex-col p-2 mb-5'>
-              <Image className='w-10 h-10 rounded-full' src={followers.user.profile_picture} height={50} width='50' />
-              <div className='text-gray-300 text-xs'>{followers.user.first_name}</div>
+          <Link href={`/profile/${followers.user.id}`}>
+            <div key={followers.user.id}>
+              <div className='flex flex-col p-2 mb-5 items-center'>
+                <Image className='w-10 h-10 rounded-full' src={followers.user.profile_picture} height={50} width='50' />
+                <div className='text-gray-300 text-xs'>{followers.user.username.substring(0, 8)}</div>
+              </div>
             </div>
-          </div>
+          </Link>
         );
       })}
     </div>
