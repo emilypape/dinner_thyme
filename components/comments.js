2f8a05cdce385d7ded0bb16e73b5ff60c653cb00
emily <eap6787@gmail.com>
2023-02-22 12:05:37 -0700

    fix comment modal styles, add replies option

--- components/comments.js
+++ components/comments.js
@@ -5,6 +5,7 @@ import Link from './Link';
 
 export default function Comments({ recipeId, setCommentOpen }) {
   const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState('');
   const router = useRouter();
 
   async function getComments() {
@@ -14,6 +15,11 @@ export default function Comments({ recipeId, setCommentOpen }) {
     setComments(commentData);
   }
 
+  const handleCommentText = (event) => {
+    setCommentText(event.target.value);
+    console.log(commentText);
+  };
+
   useEffect(() => {
     getComments();
     console.log(comments);
@@ -22,7 +28,7 @@ export default function Comments({ recipeId, setCommentOpen }) {
   return (
     <div className='fixed inset-0 z-40 animate-fade-in-up'>
       <div className='lg:justify-center md:justify-center lg:items-center md:items-center absolute inset-x-0 bottom-0  flex overflow-x-hidden overflow-y-auto fixed lg:inset-0 z-50 outline-none focus:outline-none'>
-        <div className='relative lg:w-auto lg:my-6 mx-auto lg:max-w-3xl '>
+        <div className='relative lg:w-auto lg:my-6 mx-auto lg:max-w-3xl'>
           {/*content*/}
           <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
             {/*header*/}
@@ -36,7 +42,7 @@ export default function Comments({ recipeId, setCommentOpen }) {
               <div className='hidden lg:block md:block xl:block '>
                 <Image src={comments.image_urls} width={550} height={500} />
               </div>
-              <div className='max-h-[29rem] overflow-y-scroll '>
+              <div className='max-h-[25rem] overflow-y-scroll '>
                 {comments?.comments?.map((comment) => {
                   return (
                     <div className='flex-col' id={comment.id}>
@@ -58,15 +64,25 @@ export default function Comments({ recipeId, setCommentOpen }) {
                           {comment.comment_text}
                         </div>
                       </div>
-                      <div className='ml-12 mb-2 text-xs text-gray-400'>Reply</div>
+                      <div>
+                        <div className='ml-12 mb-1 text-xs text-gray-400'>Reply</div>
+                        <div className='flex ml-12 '>
+                          <div className='ml-2 text-xs text-gray-400'>___</div>
+                          <div className='ml-2 mb-2 mt-1 text-xs text-gray-400'> View replies</div>
+                          <div className='ml-2 text-xs text-gray-400'>___</div>
+                        </div>
+                      </div>
                     </div>
                   );
                 })}
               </div>
             </div>{' '}
             {/*footer*/}
-            <div className='flex items-center justify-end border-t border-solid border-slate-200 rounded-b'>
-              <input className='appearance-none' placeholder='Add a comment...'></input>
+            <div className=' appearance-none flex items-center justify-end border-t border-solid border-slate-200 rounded-b'>
+              <input
+                className='appearance-none min-w-full lg:min-w-[42%] md:min-w-[42%]'
+                onChange={handleCommentText}
+                placeholder='Add a comment...'></input>
             </div>
           </div>
         </div>
