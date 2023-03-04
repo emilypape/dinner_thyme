8df566ff2de7f5487cc4b93b5e463546cc97a37a
emily <eap6787@gmail.com>
2023-03-04 10:59:08 -0700

    create new like route in backend

--- pages/api/newLike.js
+++ pages/api/newLike.js
@@ -4,6 +4,30 @@ import { withIronSession } from 'next-iron-session';
 async function newLike(req, res) {
   const { user } = req.session.get();
   const userId = user.user_id;
+  let idString = req.url.split('/').pop();
+  const recipeId = parseInt(idString);
+
+  if (userId && recipeId) {
+    const isLiked = await Likes.findOne({
+      where: {
+        user_id: userId,
+        recipe_id: recipeId,
+      },
+    });
+
+    if (isLiked) {
+      res.status(200).json({ message: 'This user has already liked this post' });
+    }
+
+    if (!isLiked) {
+      const newPostLike = await Likes.create({
+        recipe_id: recipe_id,
+        user_id: userId,
+      });
+
+      res.status(200).json(newPostLike);
+    }
+  }
 }
 
 export default withIronSession(newLike, {
