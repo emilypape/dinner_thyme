26aff626bb7133c34b74be3541f18213c17e4b63
emily <eap6787@gmail.com>
2023-03-12 16:21:24 -0600

    add profile links to recipe page

--- components/recipe.js
+++ components/recipe.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from './Link';
 
 export default function Recipe({ recipeId }) {
   const [recipeData, setRecipeData] = useState(false);
@@ -24,19 +25,25 @@ export default function Recipe({ recipeId }) {
         <div className='flex'>
           <div className='flex lg:ml-48 p-10'>
             <div>
-              <div className='text-center uppercase text-white bg-green-400 rounded-full p-1 text-xs max-w-[12em] mb-2'>
-                {recipeData?.user?.first_name}'s Kitchen
-              </div>
+              <Link href={`/profile/${recipeData?.user?.id}`}>
+                <div className='text-center uppercase text-white bg-green-400 rounded-full p-1 text-xs max-w-[12em] mb-2'>
+                  {recipeData?.user?.first_name}'s Kitchen
+                </div>
+              </Link>
               <div className=' font-semibold text-4xl width-[10em] max-w-[10em] mr-2'>{recipeData?.title}</div>
               <div className='mt-2 underline text-green-400 cursor-pointer mr-[5em]'>See what people are saying</div>
               <div className='flex mt-8'>
-                <Image
-                  className='shadow rounded-full max-w-full h-auto align-middle border-none'
-                  src={recipeData?.user?.profile_picture}
-                  width={40}
-                  height={40}
-                />
-                <div className='mt-1 ml-1'>{recipeData?.user?.username}</div>
+                <Link href={`/profile/${recipeData?.user?.id}`}>
+                  <Image
+                    className='shadow rounded-full max-w-full h-auto align-middle border-none'
+                    src={recipeData?.user?.profile_picture}
+                    width={40}
+                    height={40}
+                  />
+                </Link>
+                <Link href={`/profile/${recipeData?.user?.id}`}>
+                  <div className='mt-1 ml-1'>{recipeData?.user?.username}</div>
+                </Link>
               </div>
               <div className='mt-3 mb-2 flex'>
                 <div className='font-semibold text-md'>Cooking Instructions</div>
