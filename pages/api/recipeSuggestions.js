fe63367c15c1c4dcdea0c154f24352b30d7dac1e
emily <eap6787@gmail.com>
2023-04-22 21:17:53 -0600

    randomize recipe suggestions

--- pages/api/recipeSuggestions.js
+++ pages/api/recipeSuggestions.js
@@ -1,10 +1,14 @@
 const { Recipe } = require('../../database/models');
 
 export default async function recipeSuggestions(req, res) {
-  let tryTheseRecipes = await (await Recipe.findAll({ attributes: ['id', 'image_urls'] })).slice(0, 4);
+  let tryTheseRecipes = await await Recipe.findAll({ attributes: ['id', 'image_urls'] });
+
+  if (tryTheseRecipes) {
+    let recipes = [...tryTheseRecipes].sort(() => (Math.random() > 0.5 ? 1 : -1)).slice(0, 4);
+    res.status(200).json(recipes);
+  }
 
   if (!tryTheseRecipes) {
     res.status(400).json({ message: 'The request was unsuccessful' });
   }
-  res.status(200).json(tryTheseRecipes);
 }
