962f3f9ece625d709d5c51434ae11a3d3838928e
emily <eap6787@gmail.com>
2023-05-12 12:34:26 -0600

    capture new recipe information

--- components/newRecipe.js
+++ components/newRecipe.js
@@ -5,6 +5,10 @@ import TextEditor from './TextEditor';
 export default function NewRecipe() {
   const [ingredients, setIngredients] = useState([]);
   const [currentIngredient, setCurrentIngredient] = useState('');
+  const [title, setTitle] = useState('');
+  const [cookTemperature, setCookTemperature] = useState('');
+  const [cookTime, setCookTime] = useState('');
+  const [prepTime, setPrepTime] = useState('');
 
   function newIngredient(event) {
     let ingredient = event.target.value;
@@ -27,6 +31,10 @@ export default function NewRecipe() {
     ingredientArray.splice(indexToRemove, 1);
 
     setIngredients(ingredientArray);
+  }
+
+  function handleChange(e, setter) {
+    setter(e.target.value);
   }
 
   return (
@@ -63,6 +71,7 @@ export default function NewRecipe() {
         <form className='w-full container'>
           <div className=' border-b border-green-500 py-2 lg:mr-[7em] md:mr-[7em] lg:ml-[7em] md:ml-[7em]'>
             <input
+              onChange={(e) => handleChange(e, setTitle)}
               className='appearance-none bg-transparent border-none w-full text-gray-700  leading-tight focus:outline-none'
               type='text'
               placeholder='Title'
@@ -72,6 +81,7 @@ export default function NewRecipe() {
           <div className='flex justify-between mt-10 lg:mr-[7em] md:mr-[7em] lg:ml-[7em] md:ml-[7em]'>
             <div className=' border-b border-green-500 py-2 mr-1 '>
               <input
+                onChange={(e) => handleChange(e, setCookTemperature)}
                 className='appearance-none bg-transparent border-none w-full text-gray-700  leading-tight focus:outline-none'
                 type='text'
                 placeholder='Cook Temp'
@@ -80,6 +90,7 @@ export default function NewRecipe() {
             </div>
             <div className=' border-b border-green-500 py-2 mr-1'>
               <input
+                onChange={(e) => handleChange(e, setPrepTime)}
                 className='appearance-none bg-transparent border-none w-full text-gray-700  leading-tight focus:outline-none'
                 type='text'
                 placeholder='Prep Time'
@@ -88,6 +99,7 @@ export default function NewRecipe() {
             </div>
             <div className=' border-b border-green-500 py-2 '>
               <input
+                onChange={(e) => handleChange(e, setCookTime)}
                 className='appearance-none bg-transparent border-none w-full text-gray-700  leading-tight focus:outline-none'
                 type='text'
                 placeholder='Cook Time'
